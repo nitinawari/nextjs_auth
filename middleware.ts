d95b0279f765d70346d28bb0c67ffbe9c899d7a1
root <root@DESKTOP-GGAS6UD.localdomain>
2024-10-21 07:43:52 -0700

    bug fixed

--- middleware.ts
+++ middleware.ts
@@ -11,6 +11,7 @@ import {
 
 const { auth } = NextAuth(authConfig)
 export default auth((req) =>{
+  
          const { nextUrl } = req;
          const isLoggedIn  = !!req.auth;  //!! this turn null value in false 
 
